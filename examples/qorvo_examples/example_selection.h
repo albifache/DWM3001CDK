2c8c8ee34930638117080bb7b15ee45496696906
Tabrez <e.tabrez@tutamail.com>
2026-05-24 23:52:24 +0100

    Enabled device ID example

--- examples/qorvo_examples/example_selection.h
+++ examples/qorvo_examples/example_selection.h
@@ -19,7 +19,7 @@ extern "C"
 
     // Enable the needed example/test. Please enable only one example/test!
 
-//#define TEST_READING_DEV_ID
+#define TEST_READING_DEV_ID
 //#define TEST_SIMPLE_TX
 //#define TEST_SIMPLE_TX_PDOA
 //#define TEST_TX_SLEEP
